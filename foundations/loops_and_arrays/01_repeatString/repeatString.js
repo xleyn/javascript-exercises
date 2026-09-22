fdca9d80360f5175c6d646766232bfa0b1b69913
xleyn <nathanjcrossley@gmail.com>
2026-09-22 17:42:41 +0100

    Completed LAA 01 and 02

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR";
+  if (num === 0) return "";
 
+  result = string.slice();
+
+  for (let i = 0; i < num - 1; i++) {
+    result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  reverse = "";
+  for (let i = -1; -i <= str.length; i--) {
+    console.log(i);
+    reverse += str.at(i);
+  }
+  return reverse;
 };
 
 // Do not edit below this line
