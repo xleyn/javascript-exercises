67a6aa3d7f15d1e4ca330431735565ce100a35f4
xleyn <nathanjcrossley@gmail.com>
2026-09-23 11:25:42 +0100

    Completed LAA 03

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toRemove) {
+  for (elem of toRemove) {
+    let idxToRemove = arr.indexOf(elem);
+    while (idxToRemove !== -1) {
+      arr.splice(idxToRemove, 1);
+      idxToRemove = arr.indexOf(elem);
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
