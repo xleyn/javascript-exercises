cfcaec1ce37f4522305bb321ba4d6e528fa5ce52
xleyn <nathanjcrossley@gmail.com>
2026-09-23 11:58:00 +0100

    Completed LAA 06

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const abs = ((fahrenheit - 32) * 5) / 9;
+  return Math.round(abs * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const abs = (celsius * 9) / 5 + 32;
+  return Math.round(abs * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
