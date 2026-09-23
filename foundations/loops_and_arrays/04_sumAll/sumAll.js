d319cd5e57b95a6e0b0538f551a0dcce8aac7e1b
xleyn <nathanjcrossley@gmail.com>
2026-09-23 11:37:52 +0100

    Completed LAA 04

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
+  if (min > max) {
+    [max, min] = [min, max];
+  }
+  res = 0;
+  for (let i = min; i <= max; i++) {
+    res += i;
+  }
+  return res;
 };
 
 // Do not edit below this line
