932ad52121356306705cfb220217824be49aca42
xleyn <nathanjcrossley@gmail.com>
2026-09-24 18:01:16 +0100

    Completed Objects 05

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  arr.sort((a, b) => {
+    aYOD = a.yearOfDeath ? a.yearOfDeath : new Date().getFullYear();
+    bYOD = b.yearOfDeath ? b.yearOfDeath : new Date().getFullYear();
 
+    aAge = aYOD - a.yearOfBirth;
+    bAge = bYOD - b.yearOfBirth;
+    return bAge - aAge;
+  });
+  return arr[0];
 };
 
 // Do not edit below this line
